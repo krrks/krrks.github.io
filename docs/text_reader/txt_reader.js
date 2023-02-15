8486d6b728f3def129f9c6b281bd2a0a9a5303a2
github_pages <xx@xx.com>
2023-02-15 20:14:08 +0800

    123

--- docs/text_reader/txt_reader.js
+++ docs/text_reader/txt_reader.js
@@ -2,7 +2,7 @@ const inputField = document.getElementById('input-temp');
 const fromUnitField = document.getElementById('input-unit');
 const toUnitField = document.getElementById('output-unit');
 const outputField = document.getElementById('output-temp');
-const form = document.getElementById('converter');
+const form = document.getElementById('txt_reader');
 
 function convertTemp(value, fromUnit, toUnit) {
   if (fromUnit === 'c') {
